656282c9d6f78f4e3754bdeda175f69e01c1c2da
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-02 16:42:21 +0200

    add fire icon

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,11 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+    // This block will be executed once the page is loaded and ready
+
+    let hotElements = Array.from(document.querySelectorAll('.hot'));
+    
+    for(let i = 0; i < hotElements.length; i++) {
+        hotElements[i].innerHTML = hotElements[i].innerHTML + " 🔥";
+    }
 });
